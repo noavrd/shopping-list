83592554d8f992d34f07492bbb63ebf9b774c1d2
noavrd <noavrd@gmail.com>
2021-03-16 18:55:35 +0200

    inital commit

---
+++ shopping-list/src/components/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+const groceries = ["Strawberry", "Blueberry", "Orange", "Banana", "Apple", 
+"Carrot", "Celery", "Mushroom", "Green Pepper", "Eggs","Cheese", "Butter", "Chicken", "Beef", "Pork", "Fish", "Rice", "Pasta", "Bread"]
+function App() {
+  return (
+    <div className="App">
+    
+    </div>
+  );
+}
+
+export default App;
